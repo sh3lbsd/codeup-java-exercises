5a81b836461ee4521ca494cdbca54c0a8f66da65
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-10 10:32:29 -0500

    fixed warmup

---
+++ src/CatTest.java
@@ -0,0 +1,8 @@
+public class CatTest {
+    public static void main(String[] args) {
+        Cat roxy = new Cat();
+        roxy.name = "Roxy";
+        roxy.sound = "meOOOOOWWWWWWWWW";
+        System.out.println(roxy.makeNoise());
+    }
+}
