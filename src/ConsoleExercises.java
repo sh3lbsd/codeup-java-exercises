211abb15ff16cb5781fb3a3865b404639e89ee50
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-10 07:22:56 -0500

    finished console exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,28 +3,41 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.format("The value of pi is approximately %.2f.%n", pi);
+//        System.out.format("The value of pi is approximately %.2f.%n", pi);
         // %-10 might come in handy in future exercises
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n Please insert an integer ");
-        int userInput = scanner.nextInt();
-
-        System.out.println("The number you entered is " +  userInput);
-        // if you input something that's not an integer you get an InputMismatchException error
-        System.out.println("\n please enter three words");
-        String firstWord = scanner.next();
-        String secondWord = scanner.next();
-        String thirdWord = scanner.next();
-        System.out.println("you said the following three words... \n first: " + firstWord + "\n second: " + secondWord + "\n third: " + thirdWord );
-        System.out.printf("\"%s\"%n\"%s\"%n\"%s\"%n", firstWord, secondWord, thirdWord);
+//        System.out.println("\n Please insert an integer ");
+//        int userInput = scanner.nextInt();
+//
+//        System.out.println("The number you entered is " +  userInput);
+//        // if you input something that's not an integer you get an InputMismatchException error
+//        System.out.println("\n please enter three words");
+//        String firstWord = scanner.next();
+//        String secondWord = scanner.next();
+//        String thirdWord = scanner.next();
+//        System.out.println("you said the following three words... \n first: " + firstWord + "\n second: " + secondWord + "\n third: " + thirdWord );
+//        System.out.printf("\"%s\"%n\"%s\"%n\"%s\"%n", firstWord, secondWord, thirdWord);
 //        What happens if you enter less than 3 words?
 //        won't run until you type in the 3rd word
 //        What happens if you enter more than 3 words?
 //        it won't let you, it runs before u can try
-        System.out.println("please write a sentence");
-        String sentence  = scanner.nextLine();
-        sentence = scanner.nextLine();
-        System.out.printf("your sentence is: %s\n", sentence);
+//        System.out.println("Please write a sentence");
+//        String sentence  = scanner.nextLine();
+//        sentence = scanner.nextLine();
+//        System.out.printf("Your sentence is: %s\n", sentence);
+
+//        Prompt a user to enter a sentence, then store that sentence in a String variable using the next method. Then, display that sentence back to the user.
+//
+//        do you capture all the words?
+//                Rewrite the above example using the nextLine method.
+        System.out.print("Enter the length: ");
+        int lengthInput = scanner.nextInt();
+        System.out.print("Enter the width: ");
+        int widthInput = scanner.nextInt();
+        int userInput = 2 * (lengthInput + widthInput);
+        System.out.println("Perimeter of Rectangle: " + userInput);
+        int area = lengthInput * widthInput;
+        System.out.println("Area of rectangle is: " + area );
 
 
 
