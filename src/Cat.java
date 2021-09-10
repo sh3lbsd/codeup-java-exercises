db9e94786cfe4fbeb52ccfa7916969129633d85d
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-10 10:28:54 -0500

    mini exercise make a pet

---
+++ src/Cat.java
@@ -0,0 +1,17 @@
+class Cat {
+    public String name;
+    public String sound;
+
+    public String makeNoise() {
+        return String.format(name +  " goes " + sound);
+    }
+
+    public static void main(String[] args) {
+        Cat roxy = new Cat();
+        roxy.name = "Roxy";
+        roxy.sound = "meOOOOOWWWWWWWWW";
+        System.out.println(roxy.makeNoise());
+
+//    Create a class for your favorite type of animal. The class should be named for your animal - Cat, Dog, Parrot etc. It should have two instance variables - name and sound - and one method - makeNoise(). The output of the makeNoise method should be name +  " goes " + sound. Create a test class with a main method to instantiate and test your class code.
+    }
+}
