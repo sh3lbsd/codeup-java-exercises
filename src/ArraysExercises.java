efa893295192324c2c6d3420a1a043b7fcad030e
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-13 10:26:59 -0500

    started java arrays exercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,7 @@
+public class ArraysExercises {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+        }
+
+    }
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,16 @@
+package movies;
+
+import util.Input;
+
+//Give the user a list of options for viewing all the movies or viewing movies by category.
+//If a category is selected, only movies from that category should be displayed.
+//        Your application should continue to run until the user chooses to exit.
+//Bonus: Add functionality to allow a user to add a new movie to the list.
+public class MoviesApplication {
+
+    public static void main(String[] args) {
+//        Use your Input class to get input from the user, and display information based on their choice. (Remember to import your Input class)
+        Input input = new util.Input();
+
+    }
+}
