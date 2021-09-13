316a5ee7f2152db65f2205c2048bfcf38baf290d
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-13 16:48:20 -0500

    okay finished part one of array and added a person class

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,13 +1,6 @@
 import java.util.Arrays;
 
-//    Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
-public class ArraysExercises extends Person {
-    public static Person[] addPerson(Person[] arr, Person addPerson) {
-        Person[] newArray = Arrays.copyOf(arr, arr.length + 1);
-        newArray[arr.length] = addPerson;
-        return newArray;
-    }
-
+public class ArraysExercises {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         System.out.println(numbers);
@@ -32,5 +25,11 @@ public class ArraysExercises extends Person {
         for (Person person : people) {
             System.out.println(person.getName());
         }
+    }
+    //      Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+    public static Person[] addPerson(Person[] arr, Person addPerson) {
+        Person[] newArray = Arrays.copyOf(arr, arr.length + 1);
+        newArray[arr.length] = addPerson;
+        return newArray;
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    private String name;
+
+    public String getName(){
+//TODO: return the person's name
+        return name;
+    }
+
+    public void setName(String name){
+//TODO: change the name property to the passed value
+        this.name = name;
+
+
+    }
+    public void sayHello(){
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello " + name);
+    }
+    public Person(String name) {
+        this.name = name;
+    }
+    public Person(){}
+
+    public static void main(String[] args) {
+        Person testPerson = new Person("Mojojo");
+        System.out.println(testPerson.getName());
+        Person person1 = new Person("Jo");
+        Person person2 = new Person("JoJoJoJo");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("NoodleHead");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+    }
+}
