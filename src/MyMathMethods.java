0529516a6f7f66a2a192b9b8e3da9d46c922d2ab
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-09-13 09:28:10 -0500

    did warmup

---
+++ src/MyMathMethods.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+//WARMUP 09/13/21
+//Create a class called MyMathMethods
+//        Create a method in there called multiply
+//Have it take two integer parameters and return the product of the two integers
+//Do not create a main method in your MyMathMethods class
+//Instead, create a test class, and run your multiply method from a main method in your test class
+//If you want a bonus, you can overload the method with a version that takes doubles instead of ints as its parameters
+//As another bonus … after you’re done, refactor so the multiply method is static, then refactor your test so as to use the static method
+// another bonus: getInput from the user
+public class MyMathMethods {
+
+    public static int multiply(int num1, int num2){
+        return num1 * num2;
+    }
+
+}
---
+++ src/MyMathMethodsTest.java
@@ -0,0 +1,11 @@
+public class MyMathMethodsTest {
+
+    public static int mathTestInt = -23;
+    public static int absoluteValueTest = Math.abs(mathTestInt);
+    public static void main(String[] args) {
+//        System.out.println(absoluteValueTest);
+//        MyMathMethods mathMethods = new MyMathMethods();
+//        System.out.println(mathMethods.multiply(4, 5));
+        System.out.println(MyMathMethods.multiply(5,5));
+    }
+}
