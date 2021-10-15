9cd25aaf983464a70371cf76d8b610a47d0affcf
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2021-10-15 17:15:32 -0500

    mini-warmup.. unfinished

--- src/miniExercisesWarmups/JavaDrills.java
+++ src/miniExercisesWarmups/JavaDrills.java
@@ -24,6 +24,8 @@ public class JavaDrills {
         return new String(chars);
     }
 
+    public static returnTotalDifference()
+
     public static void main(String[] args) {
         System.out.println(flipOuterCase("cat")); // CaT
         System.out.println(flipOuterCase("CaT")); // cat
@@ -32,3 +34,27 @@ public class JavaDrills {
     }
 
 }
+//================================= WARM UP
+//        In `codeup-java-exercises`, in `JavaDrills`:
+//        Create a static method, returnTotalDifference, that takes in two lists of integers and returns the difference between the sum of all integers in the first list minus the sum of all integers in the second list.
+//        returnTotalDifference(Arrays.asList(10, 2, 3), Arrays.asList(1, 2, 3)); // returns 9
+//        returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7)); // returns 3
+
+    public static int returnTotalDifference(List<Integer> integerListOne, List<Integer> integerListTwo){
+    int sumOfOne = 0;
+    int sumOfTwo = 0;
+
+    for (int integer : integerListOne){
+      sumOfOne += integer;
+    }
+    for (int integer: integerListTwo){
+        sumOfTwo += integer;
+    }
+    return sumOfOne - sumOfTwo;
+}
+
+
+    public static void main(String[] args) {
+        System.out.println(returnTotalDifference(Arrays.asList(10,2,3),Arrays.asList(1,2,3)));
+}
+}    returnTotalDifference(Arrays.asList(10, 1), Arrays.asList(1, 7, 1)); // returns 2
